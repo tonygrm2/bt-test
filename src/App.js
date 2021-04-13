b3d59154301b0938f041ab74e0d5c7e66fccd149
TonyGrm <tony@grimaldidev.com>
2021-04-13 09:01:06 +0100

    render bt logo and test author

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import btLogo from './assets/bt-logo.png';
 import { NewsApp } from './NewsApp/Container/NewsApp';
 
 function App() {
   return (
     <div className="App">
+      <h1>BT Test Antonino Grimaldi</h1>
+      <img src={btLogo} alt="bt-logo" className="bt-logo"/>
       <NewsApp />
     </div>
   );
