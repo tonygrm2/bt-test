319e5eb0def5504ac870fe1aa39a64117aa47a32
TonyGrm <tony@grimaldidev.com>
2021-04-13 13:22:26 +0100

    create header and footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { NewsApp } from './NewsApp/Container/NewsApp';
 
 function App() {
   return (
-    <div className="App">
-      <h1>BT Test Antonino Grimaldi</h1>
+    <div className="app">
+      <header>
+        <h1>BT Test Antonino Grimaldi</h1>
+      </header>
       <img src={btLogo} alt="bt-logo" className="bt-logo"/>
       <NewsApp />
     </div>
---
+++ src/NewsApp/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Footer = () => (
+  <div className="footer-container">
+    <footer>
+      <table className="footer-table">
+        <tbody>
+          <tr>
+            <td className="footer-element">Lorem Ipsum</td>
+            <td className="footer-element">Lorem Ipsum</td>
+            <td className="footer-element">Lorem Ipsum</td>
+          </tr>
+        </tbody>
+        </table>
+        <p className="copyright">Antonino Grimaldi © 2021</p>
+    </footer>
+  </div>
+)
